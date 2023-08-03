3e43f38153660a5ae6a6e8b82f353aeadc84769f
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-03 23:05:27 +0500

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./style.css";
+import * as React from 'react';
+import './style.css';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[-10, -10, -10]} angle={0.15} />
+      <pointLight position={[-10, -10, -10]} />
+      <ModelViewer modelPath="../public/art_gallery.glb" />
+    </Canvas>
   );
 }
