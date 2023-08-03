89c240e625be14c2a6c4c4372b4004fdeed5e82d
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-03 23:40:41 +0500

    1 file modified

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import ModelComp from './ModelComp';
 
-const ModelViewer = ({ modelPath, scale, position }) => {
+const ModelViewer = ({ modelPath, scale = 10, position = [0, 0, 0] }) => {
   return (
     <Suspense fallback={null}>
       <ModelComp modelPath={modelPath} scale={scale} position={position} />
