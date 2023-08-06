7566f94c3c0ca8b58dea7b91602a5e3aa9855e5f
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-06 14:52:06 +0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
+// import React from 'react';
+// import { OrbitControls } from '@react-three/drei';
+// import { Canvas } from '@react-three/fiber';
+// import ModelViewer from './ModelViewer';
+
+// import './style.css';
+
+// export default function App() {
+//   return (
+//     <Canvas>
+//       <OrbitControls />
+//       {/* Stars component is missing, make sure to add it if needed */}
+//       <ambientLight intensity={0.5} />
+//       <spotLight position={[-10, -10, -10]} angle={0.15} />
+//       <pointLight position={[-10, -10, -10]} />
+//       <ModelViewer modelPath="../public/art_gallery.glb" />
+//     </Canvas>
+//   );
+// }
+
 import React from 'react';
-import { OrbitControls } from '@react-three/drei';
+import './App.css';
+
+//three.js
 import { Canvas } from '@react-three/fiber';
-import ModelViewer from './ModelViewer';
+import { OrbitControls } from '@react-three/drei';
+import Model from './model/Artgallerymodel';
 
-import './style.css';
-
-export default function App() {
+function App() {
   return (
-    <Canvas>
-      <OrbitControls />
-      {/* Stars component is missing, make sure to add it if needed */}
-      <ambientLight intensity={0.5} />
-      <spotLight position={[-10, -10, -10]} angle={0.15} />
-      <pointLight position={[-10, -10, -10]} />
-      <ModelViewer modelPath="../public/art_gallery.glb" />
-    </Canvas>
+    <div className="App">
+      <header className="App-header">
+        <Canvas className="canvas">
+          <Model /> //put it here!
+          <ambientLight intensity={1} />
+          <directionalLight position={[-2, 5, 2]} intensity={1} />
+          <OrbitControls enableZoom={false} autoRotate={true} />
+        </Canvas>
+      </header>
+    </div>
   );
 }
+
+export default App;
